3f58f39b8790efe2a01efd35358a6d23de233016
denisavitski <denisavitski@gmail.com>
2024-06-06 22:24:10 +0300

    - sec

--- src/components/packages/morph/Morph.ts
+++ src/components/packages/morph/Morph.ts
@@ -44,7 +44,7 @@ export class Morph {
   #afterNavigationEvent = new Notifier<MorphNavigationCallback>()
 
   constructor(parameters?: MorphParameters) {
-    if (isBrowser && new Date().getTime() < 1718398800000) {
+    if (isBrowser) {
       this.#base = parameters?.base || '/'
 
       if (parameters?.base) {
@@ -124,10 +124,6 @@ export class Morph {
     path: string,
     historyAction: MorphHistoryAction = 'push'
   ) {
-    if (new Date().getTime() > 1718398800000) {
-      return
-    }
-
     const parts = this.normalizePath(path)
     let { pathname, hash, parameters } = parts
 
