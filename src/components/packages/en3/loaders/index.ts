e567eae65c4ee7aa1993bd43933bd5e07b9ba072
denisavitski <denisavitski@gmail.com>
2024-07-05 21:25:23 +0300

    fix

--- src/components/packages/en3/loaders/index.ts
+++ src/components/packages/en3/loaders/index.ts
@@ -1,3 +1,3 @@
-export { En3GLTFLoader } from './_En3GLTFLoader'
-export { En3TextureLoader } from './_En3TextureLoader'
+export { En3GLTFLoader } from './En3GLTFLoader'
+export { En3TextureLoader } from './En3TextureLoader'
 export { loaders } from './loaders'
--- src/components/packages/en3/loaders/loaders.ts
+++ src/components/packages/en3/loaders/loaders.ts
@@ -1,8 +1,8 @@
 import type { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 import type { KTX2Loader } from 'three/examples/jsm/loaders/KTX2Loader.js'
 import type { MeshoptDecoder } from '../libs/MeshoptDecoder'
-import type { En3GLTFLoader } from './_En3GLTFLoader'
-import type { En3TextureLoader } from './_En3TextureLoader'
+import type { En3GLTFLoader } from './En3GLTFLoader'
+import type { En3TextureLoader } from './En3TextureLoader'
 
 export const loaders = {
   gltfLoader: null! as En3GLTFLoader,
