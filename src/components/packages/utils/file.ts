7d90a92e478a3ea0a025a90f30fb54a982dd5eac
denisavitski <denisavitski@gmail.com>
2024-09-30 02:30:08 +0300

    utils: downloadURI

--- src/components/packages/utils/file.ts
+++ src/components/packages/utils/file.ts
@@ -1,10 +1,16 @@
 export function createJSONAndSave(filename: string, dataObjToWrite: object) {
-  const blob = new Blob([JSON.stringify(dataObjToWrite)], { type: 'application/json' })
+  const blob = new Blob([JSON.stringify(dataObjToWrite)], {
+    type: 'application/json',
+  })
   const link = document.createElement('a')
 
   link.download = filename + '.json'
   link.href = window.URL.createObjectURL(blob)
-  link.dataset.downloadurl = ['application/json', link.download, link.href].join(':')
+  link.dataset.downloadurl = [
+    'application/json',
+    link.download,
+    link.href,
+  ].join(':')
 
   const evt = new MouseEvent('click', {
     view: window,
@@ -13,5 +19,21 @@ export function createJSONAndSave(filename: string, dataObjToWrite: object) {
   })
 
   link.dispatchEvent(evt)
+  link.remove()
+}
+
+export function downloadURI(uri: string, name?: string) {
+  const link = document.createElement('a')
+
+  if (name) {
+    link.setAttribute('download', name)
+  }
+
+  link.href = uri
+
+  document.body.appendChild(link)
+
+  link.click()
+
   link.remove()
 }
