f3a75079a5ede0487872242bfe747177518ff07d
denisavitski <denisavitski@gmail.com>
2025-01-07 06:34:40 +0300

    utils: declension

--- src/components/packages/utils/string.ts
+++ src/components/packages/utils/string.ts
@@ -94,3 +94,32 @@ export function decodeHtmlEntities(html: string) {
   textarea.innerHTML = html
   return textarea.value
 }
+
+export function declension(
+  number: number | string,
+  one: string,
+  few: string,
+  many: string
+) {
+  const n = typeof number === 'string' ? parseInt(number) : number
+
+  if (n === 1) {
+    return one
+  }
+
+  if (n % 100 >= 11 && n % 100 <= 14) {
+    return many
+  }
+
+  const remainder = n % 10
+
+  if (remainder === 1) {
+    return one
+  }
+
+  if (remainder >= 2 && remainder <= 4) {
+    return few
+  }
+
+  return many
+}
