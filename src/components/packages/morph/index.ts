aee4c23f0b4b228d6807009589d6f0f61d367b89
denisavitski <denisavitski@gmail.com>
2025-02-26 08:38:40 +0300

    morph: убрал лишние импорты

--- src/components/packages/morph/index.ts
+++ src/components/packages/morph/index.ts
@@ -1,11 +1,9 @@
 export {
   Morph,
-  type MorphNavigationCallback,
   type MorphPreprocessor,
   type MorphPreprocessorEntry,
   type MorphNavigationEntry,
   type MorphOptions,
-  type MorphTransitionEntry,
   type MorphChildrenActionEntry,
   type MorphNavigateOptions,
   type MorphEvents,
