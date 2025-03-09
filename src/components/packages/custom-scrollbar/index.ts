2eb7a02ab4a292c653b57037b91b458ddbdfa35a
denisavitski <denisavitski@gmail.com>
2025-03-09 18:14:34 +0300

    custom-scrollbar: formula fix, horisontal attr

--- src/components/packages/custom-scrollbar/index.ts
+++ src/components/packages/custom-scrollbar/index.ts
@@ -1,26 +1,29 @@
-/**
- * TODO horisontal
- */
-
-import { elementResizer } from '@packages/element-resizer'
-import { windowResizer } from '@packages/window-resizer'
 import { findScrollParentElement, isBrowser, setupDrag } from '@packages/utils'
 import { ticker } from '@packages/ticker'
+import { intersector } from '@packages/intersector'
 
 export class CustomScrollbarElement extends HTMLElement {
   #scrollElement: HTMLElement = null!
   #thumbElement: HTMLElement = null!
   #thumbScrollSize = 0
   #limit = 0
+  #isHorizontal: boolean = false
 
   #activeTimeoutId: ReturnType<typeof setTimeout> | undefined
 
   protected connectedCallback() {
     const scrollSelector = this.getAttribute('data-scroll')
+
+    this.#isHorizontal = this.getAttribute('orientation') === 'horizontal'
+
     let scrollElement: HTMLElement | null = null
 
     if (scrollSelector) {
-      scrollElement = document.querySelector(scrollSelector)
+      if (scrollSelector === 'parent') {
+        scrollElement = this.parentElement
+      } else {
+        scrollElement = document.querySelector(scrollSelector)
+      }
     }
 
     if (!scrollElement) {
@@ -33,25 +36,20 @@ export class CustomScrollbarElement extends HTMLElement {
       this.#scrollElement = scrollElement
       this.#thumbElement = thumbElement
 
-      elementResizer.subscribe(this.#scrollElement, this.#resizeListener)
-      windowResizer.subscribe(this.#resizeListener)
-
       this.#scrollElement.addEventListener('scroll', this.#scrollListener)
       this.#thumbElement.addEventListener('pointerdown', this.#grabListener)
 
-      ticker.subscribe(this.#tickListener, { maxFPS: 5 })
+      intersector.subscribe(this.parentElement!, this.#intersectionListener)
     }
   }
 
   protected disconnectedCallback() {
-    elementResizer.unsubscribe(this.#resizeListener)
-    windowResizer.unsubscribe(this.#resizeListener)
-
     this.#scrollElement.removeEventListener('scroll', this.#scrollListener)
     this.#thumbElement.removeEventListener('pointerdown', this.#grabListener)
 
     clearTimeout(this.#activeTimeoutId)
 
+    intersector.unsubscribe(this.#intersectionListener)
     ticker.unsubscribe(this.#tickListener)
   }
 
@@ -60,10 +58,17 @@ export class CustomScrollbarElement extends HTMLElement {
 
     clearTimeout(this.#activeTimeoutId)
 
-    const position =
-      (this.#scrollElement.scrollTop / this.#limit) * this.#thumbScrollSize
+    const scrollPosition = this.#isHorizontal
+      ? this.#scrollElement.scrollLeft
+      : this.#scrollElement.scrollTop
 
-    this.#thumbElement.style.transform = `translate3d(0px, ${position}px, 0px)`
+    const position = (scrollPosition / this.#limit) * this.#thumbScrollSize
+
+    if (this.#isHorizontal) {
+      this.#thumbElement.style.transform = `translate3d(${position}px, 0px, 0px)`
+    } else {
+      this.#thumbElement.style.transform = `translate3d(0px, ${position}px, 0px)`
+    }
 
     this.#activeTimeoutId = setTimeout(() => {
       this.classList.remove('active')
@@ -71,51 +76,82 @@ export class CustomScrollbarElement extends HTMLElement {
   }
 
   #resizeListener = () => {
-    const barSize = this.offsetHeight
+    const barSize = this.#isHorizontal ? this.offsetWidth : this.offsetHeight
 
-    this.#limit =
-      this.#scrollElement.scrollHeight - this.#scrollElement.offsetHeight
+    this.#limit = this.#isHorizontal
+      ? this.#scrollElement.scrollWidth - this.#scrollElement.offsetWidth
+      : this.#scrollElement.scrollHeight - this.#scrollElement.offsetHeight
 
-    let thumbSize = barSize / (this.#limit / barSize)
+    let thumbSize = (barSize * barSize) / (this.#limit + barSize)
 
     thumbSize = Math.max(thumbSize, 30)
 
-    this.#thumbElement.style.width = '100%'
-    this.#thumbElement.style.height = thumbSize + 'px'
+    if (this.#isHorizontal) {
+      this.#thumbElement.style.width = thumbSize + 'px'
+      this.#thumbElement.style.height = '100%'
+    } else {
+      this.#thumbElement.style.width = '100%'
+      this.#thumbElement.style.height = thumbSize + 'px'
+    }
 
     this.#thumbScrollSize = barSize - thumbSize
 
     if (
-      !(this.#scrollElement.scrollHeight > this.#scrollElement.offsetHeight)
+      (this.#isHorizontal &&
+        !(this.#scrollElement.scrollWidth > this.#scrollElement.offsetWidth)) ||
+      (!this.#isHorizontal &&
+        !(this.#scrollElement.scrollHeight > this.#scrollElement.offsetHeight))
     ) {
       this.style.display = 'none'
+    } else {
+      this.style.display = 'block'
     }
   }
 
   #grabListener = (grabEvent: PointerEvent) => {
     document.documentElement.classList.add('grabbing')
 
+    const startValue = this.#isHorizontal
+      ? this.#scrollElement.scrollLeft
+      : this.#scrollElement.scrollTop
+    const grabCursor = this.#isHorizontal ? grabEvent.x : grabEvent.y
+
     setupDrag(
       (moveEvent: PointerEvent) => {
         const mult = this.#limit / this.#thumbScrollSize
-        const delta = (moveEvent.y - grabCursor) * mult
+        const delta =
+          (this.#isHorizontal
+            ? moveEvent.x - grabCursor
+            : moveEvent.y - grabCursor) * mult
 
-        this.#scrollElement.scroll({
-          top: startValue + delta,
-          behavior: 'instant',
-        })
+        if (this.#isHorizontal) {
+          this.#scrollElement.scroll({
+            left: startValue + delta,
+            behavior: 'instant',
+          })
+        } else {
+          this.#scrollElement.scroll({
+            top: startValue + delta,
+            behavior: 'instant',
+          })
+        }
       },
       () => {
         document.documentElement.classList.remove('grabbing')
       }
     )
-
-    const startValue = this.#scrollElement.scrollTop
-    const grabCursor = grabEvent.y
   }
 
   #tickListener = () => {
     this.#resizeListener()
+  }
+
+  #intersectionListener = (e: IntersectionObserverEntry) => {
+    if (e.isIntersecting) {
+      ticker.subscribe(this.#tickListener, { maxFPS: 5 })
+    } else {
+      ticker.unsubscribe(this.#tickListener)
+    }
   }
 }
 
