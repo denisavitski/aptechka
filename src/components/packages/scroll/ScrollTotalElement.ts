1f5057fc50cc1048a3466334f40ccb56f6540a1a
denisavitski <denisavitski@gmail.com>
2025-06-19 05:07:25 +0300

    scroll: sectionsChangeListener

--- src/components/packages/scroll/ScrollTotalElement.ts
+++ src/components/packages/scroll/ScrollTotalElement.ts
@@ -5,15 +5,28 @@ export class ScrollTotalElement extends ScrollUserElement {
   protected override async connectedCallback() {
     super.connectedCallback()
 
-    this.scrollElement.counter.subscribe(this.#counterChangeListener)
+    this.scrollElement.sectionsInViewCSSProperty.subscribe(
+      this.#sectionsChangeListener
+    )
+    this.scrollElement.addEventListener(
+      'scrollSectionsChange',
+      this.#sectionsChangeListener
+    )
+    this.#sectionsChangeListener()
   }
 
   protected disconnectedCallback() {
-    this.scrollElement.counter.unsubscribe(this.#counterChangeListener)
+    this.scrollElement.sectionsInViewCSSProperty.unsubscribe(
+      this.#sectionsChangeListener
+    )
+    this.scrollElement.removeEventListener(
+      'scrollSectionsChange',
+      this.#sectionsChangeListener
+    )
   }
 
-  #counterChangeListener = () => {
-    const value = this.scrollElement.limit
+  #sectionsChangeListener = () => {
+    const value = this.scrollElement.limit + 1
     const pad = parseInt(this.getAttribute('pad') || '0')
     this.textContent = value.toString().padStart(pad, '0')
   }
