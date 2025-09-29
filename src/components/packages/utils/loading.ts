1c816d3b776ec8b5a9adf4fe51f48f5a9a6e644b
denisavitski <denisavitski@gmail.com>
2025-09-29 09:17:17 +0300

    utils: loadWithDelay

--- src/components/packages/utils/loading.ts
+++ src/components/packages/utils/loading.ts
@@ -1,8 +1,9 @@
 export function requestLoadingCallback(
-  event: 'DOMContentLoaded' | 'load' | 'idle',
+  event: 'DOMContentLoaded' | 'load' | 'idle' | 'loadWithDelay',
   callback: Function,
 ) {
   let idleId: ReturnType<typeof requestIdleCallback> | undefined
+  let timeoutId: ReturnType<typeof setTimeout> | undefined
 
   const domListener = () => {
     window.removeEventListener('DOMContentLoaded', loadListener)
@@ -19,13 +20,26 @@ export function requestLoadingCallback(
     callback()
   }
 
+  const delayListener = () => {
+    window.removeEventListener('load', loadListener)
+
+    timeoutId = setTimeout(() => {
+      requestAnimationFrame(() => {
+        callback()
+      })
+    }, 1000)
+  }
+
   const clear = () => {
     window.removeEventListener('DOMContentLoaded', domListener)
     window.removeEventListener('load', loadListener)
+    window.removeEventListener('load', delayListener)
 
     if (idleId && window.cancelIdleCallback) {
       window.cancelIdleCallback(idleId)
     }
+
+    clearTimeout(timeoutId)
   }
 
   if (event === 'idle' && window.requestIdleCallback) {
@@ -36,6 +50,12 @@ export function requestLoadingCallback(
         loadListener()
       } else {
         window.addEventListener('load', loadListener)
+      }
+    } else if (event === 'loadWithDelay') {
+      if (document.readyState === 'complete') {
+        delayListener()
+      } else {
+        window.addEventListener('load', delayListener)
       }
     } else if (event === 'DOMContentLoaded') {
       window.addEventListener('DOMContentLoaded', domListener)
